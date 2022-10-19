ba2a93d26ff402cf6953c371ff903e421f196054
Steven Green <ste2144105@gmail.com>
2022-10-19 15:08:48 -0700

    setup app.post

--- server.js
+++ server.js
@@ -21,3 +21,38 @@ app.get('/api/notes', (req, res)=>{
     console.info(`${req.method} request received for notes`);
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
+
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedData = JSON.parse(data);
+        parsedData.push(content)
+        writeToFile(file, parsedData);
+      }
+    });
+};
+
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to add a note ${req.body}`);
+    
+    const { title, text } = req.body;
+    
+    if (req.body) {
+      const newNote = {
+        title,
+        text,
+        id: uuid.v4(),
+      };
+      readAndAppend(newNote, './db/db.json');
+      res.json(newNote);
+    } else {
+      res.error('Error in adding note');
+    }
+});
