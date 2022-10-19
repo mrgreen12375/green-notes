a7d587bac910f64210e91e7537eadc7507bc5a05
Steven Green <ste2144105@gmail.com>
2022-10-19 15:02:57 -0700

    setup app.get

--- server.js
+++ server.js
@@ -12,3 +12,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
+
+app.get('/notes', (req, res) =>{
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('/api/notes', (req, res)=>{
+    console.info(`${req.method} request received for notes`);
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
