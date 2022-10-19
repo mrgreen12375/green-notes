b9604b860ef5f803271a27ff64203f4746efecc8
Steven Green <ste2144105@gmail.com>
2022-10-19 16:05:50 -0700

    added comments

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//setup required packages
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
@@ -5,28 +6,35 @@ const util = require('util');
 const readFromFile = util.promisify(fs.readFile);
 const uuid = require('uuid');
 
+//setup port
 const PORT = process.env.PORT || 3001;
 
+//setup connection to express
+//setup parsing json and urlencoded form data
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//setup static files from tthe public folder
 app.use(express.static('public'));
 
+//setup route to the notes.html
 app.get('/notes', (req, res) =>{
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
+//setup route to get all the notes
 app.get('/api/notes', (req, res)=>{
     console.info(`${req.method} request received for notes`);
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
+//setup function to write data to json and give a destination
 const writeToFile = (destination, content) =>
   fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
     err ? console.error(err) : console.info(`\nData written to ${destination}`)
   );
-
+//setup function to read data from file and appent some content
 const readAndAppend = (content, file) => {
     fs.readFile(file, 'utf8', (err, data) => {
       if (err) {
@@ -39,6 +47,7 @@ const readAndAppend = (content, file) => {
     });
 };
 
+//setup post request to let client know their post request was received
 app.post('/api/notes', (req, res) => {
     console.info(`${req.method} request received to add a note ${req.body}`);
     
@@ -57,6 +66,7 @@ app.post('/api/notes', (req, res) => {
     }
 });
 
+//setup function to delete notes
 app.delete("/api/notes/:id", function(req, res) {
     console.log("params", req.params.id)
     fs.readFile('./db/db.json', 'utf-8',(err,data)=>{
@@ -70,11 +80,13 @@ app.delete("/api/notes/:id", function(req, res) {
       }
     })
   });
-  
-  app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-  );
 
-  app.listen(PORT, () =>
-  console.log(`APP listening at http://localhost:${PORT}`)
+//setup wildcard route to the index.html
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+//setup to listen on port 3001
+app.listen(PORT, () =>
+    console.log(`APP listening at http://localhost:${PORT}`)
 );
