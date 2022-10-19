ba04f1a9fa39b9970366e1edf09038d18cb1e4ab
Steven Green <ste2144105@gmail.com>
2022-10-19 14:29:20 -0700

    setup app.use

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const util = require('util');
+const readFromFile = util.promisify(fs.readFile);
 const uuid = require('uuid');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
