a183b86559b0c52ee1cdeca1b0df3729ed3a102e
Steven Green <ste2144105@gmail.com>
2022-10-19 13:59:43 -0700

    setup required packages and port

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-//initial commit
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const util = require('util');
+const uuid = require('uuid');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
